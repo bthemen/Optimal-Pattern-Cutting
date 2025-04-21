4f07a3b65ba64e580384460264c0b3e89ce16bd5
Bas Themen <bthemen97@gmail.com>
2025-04-21 15:46:40 +0200

    Fitness scaling calculation

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -1,11 +1,13 @@
 # Import libraries
 import random
+import numpy as np
 
 ## Parameters
 # Genetic algorithm parameters
 POPULATION_SIZE = 100   # Population per generation
 GENOME_LENGTH   = 20    # Number of design variables per genome
 GENERATIONS     = 200   # Number of generations in evolution
+ELITE_SIZE      = 5     # Number of elite genomes to be carried over
 CROSSOVER_RATE  = 0.7   # Percentage chance of a genome crossover
 MUTATION_RATE   = 0.01  # Percentage chance of a genome mutation
 
@@ -19,8 +21,8 @@ WS_TOLERANCE    = 10    # Tolerance workspace overlap [mm]
 # Create a random genome
 def random_genome(length):
     # Create random values for the x and y coordinates of the pattern piece centers
-    random_value_x = [WS_WIDTH * random.randint(0, 1) for _ in range(length // 2)]
-    random_value_y = [WS_HEIGHT * random.randint(0, 1) for _ in range(length // 2)]
+    random_value_x = [WS_WIDTH * random.uniform(0, 1) for _ in range(length // 2)]
+    random_value_y = [WS_HEIGHT * random.uniform(0, 1) for _ in range(length // 2)]
     # TODO: add rotation of pattern pieces
 
     # Fill genome
@@ -59,6 +61,21 @@ def fitness(genome):
 
     return effective_area
 
+# Calculate fitness scaling
+def fitness_scaling(fitness_values, parents_number):
+    # Get the indices that would sort the list
+    sorted_indices = sorted(range(len(fitness_values)), key=lambda i: fitness_values[i])
+
+    # Create expectation values
+    size = len(fitness_values)
+    expectation = np.zeros(size)
+    expectation[sorted_indices] = 1 / np.sqrt(np.arange(1, size + 1))
+
+    # Scale with number of parents
+    expectation = parents_number * expectation / sum(expectation)
+
+    return expectation
+
 # Select parent
 def select_parent(population, fitness_values):
     ## TODO: does not work for this use case, try @selectionroulette as in MATLAB
@@ -69,3 +86,9 @@ def select_parent(population, fitness_values):
         current += fitness_value
         if current > pick:
             return individual
+        
+pop = init_population(POPULATION_SIZE, GENOME_LENGTH)
+fitness_values = []
+for individual in pop:
+    fitness_values.append(fitness(individual))
+scaling = fitness_scaling(fitness_values, 2)
